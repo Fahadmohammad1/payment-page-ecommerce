807018bae45b592950e74c5a3785c0be2e4f3b14
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-08-02 02:27:45 +0600

    react icon installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Payment from "./Components/Payment/Payment";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Payment />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Payment/Payment.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Payment = () => {
+  return (
+    <section>
+      <div>
+        <h3>Shipping and Payment</h3>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default Payment;
