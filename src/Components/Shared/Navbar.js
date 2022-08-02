c9555457c262562e68f492bc209b0f4794d8f5f3
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-08-02 14:36:31 +0600

    navbar added

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../Assets/images/logo.png";
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav>
       <div>
-        <div>
-          <h2 className="nav-logo">
-            <img src={logo} alt="" />
-            <span className="nav-title">E</span> -Shop
-          </h2>
-        </div>
+        <h2 className="nav-logo">
+          <img src={logo} alt="" />
+          <span className="nav-title">E-</span>Shop
+        </h2>
+      </div>
+      <div>
+        <a href=".">Men</a>
+        <a href=".">Women</a>
+        <a href=".">Kids</a>
+      </div>
+      <div className="nav-icons">
+        <FaSearch />
+        <FaShoppingCart />
+        <FaUserAlt />
       </div>
     </nav>
   );
